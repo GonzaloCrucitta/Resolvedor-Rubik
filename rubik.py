7921084e83593d514a560034ff5b926255cfb2eb
GonzaloCrucitta <gonzalocrucitta@uca.edu.ar>
2025-11-08 23:12:39 -0300

    initial commit

---
+++ rubik.py
@@ -0,0 +1,19 @@
+""" 
+Caras:
+Front: white
+Right: green
+Up:    red
+Left:  blue
+Down:  orange
+Back:  yellow
+
+Arreglo de posiciones:
+ Tengo en cuenta solamente las aristas y las esquinas
+
+ Ordenado
+esquinas[1,2,3,4,5,6,7,8] + aristas[1,2,3,4,5,6,7,8,9,10,11,12]
+
+esquinas: [ryb,rgy,rwg,rbw, oby,oyg,ogw,owb]
+
+aristas: []
+"""
