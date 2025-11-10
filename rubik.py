97bc55dc94086663682b03dc3e281a1091d87709
GonzaloCrucitta <gonzalocrucitta@uca.edu.ar>
2025-11-09 23:25:09 -0300

    implementacion de todos los giros

--- rubik.py
+++ rubik.py
@@ -1,19 +1,35 @@
-""" 
-Caras:
-Front: white
-Right: green
-Up:    red
-Left:  blue
-Down:  orange
-Back:  yellow
+import girosv2
 
-Arreglo de posiciones:
- Tengo en cuenta solamente las aristas y las esquinas
+cubo = {
+        "esquinas_pos": [0,1,2,3,4,5,6,7],
+        "esquinas_ori": [0,0,0,0,0,0,0,0],
+        "aristas_pos": [0,1,2,3,4,5,6,7,8,9,10,11],
+        "aristas_ori": [0,0,0,0,0,0,0,0,0,0,0,0]
+    }
 
- Ordenado
-esquinas[1,2,3,4,5,6,7,8] + aristas[1,2,3,4,5,6,7,8,9,10,11,12]
+nodo ={
+    "ultimo_giro", #U,R,2L,Fp, ...
+    "cubo",
+    "nodos_Sig"  #[]
+}
 
-esquinas: [ryb,rgy,rwg,rbw, oby,oyg,ogw,owb]
 
-aristas: [ry,rg,rw,rb, gy,gw,wb,by, oy,og,ow,ob]
-"""
+def print_cube(cubo, giro):
+    print("=== ",giro," ===")
+    print("Esquinas:")
+    for i, (p, o) in enumerate(zip(cubo["esquinas_pos"],cubo["esquinas_ori"])):
+        print(f"  {i}: pos={p}, ori={o}")
+    """ print("\nAristas:")
+    for i, (p, o) in enumerate(zip(cubo["aristas_pos"], cubo["aristas_ori"])):
+        print(f"  {i}: pos={p}, ori={o}")
+    print("=================") """
+
+print_cube(cubo,"")
+girosv2.B(cubo["esquinas_pos"],cubo["esquinas_ori"],cubo["aristas_pos"], cubo["aristas_ori"])
+print_cube(cubo,"B")
+girosv2.Bp(cubo["esquinas_pos"],cubo["esquinas_ori"],cubo["aristas_pos"], cubo["aristas_ori"])
+print_cube(cubo,"Bp")
+""" girosv2.F(cubo["esquinas_pos"],cubo["esquinas_ori"],cubo["aristas_pos"], cubo["aristas_ori"])
+print_cube(cubo,"F")
+girosv2.F(cubo["esquinas_pos"],cubo["esquinas_ori"],cubo["aristas_pos"], cubo["aristas_ori"])
+print_cube(cubo,"F") """
